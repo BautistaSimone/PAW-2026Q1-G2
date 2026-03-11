d0db434f3b68cfd0255e9fe35995d1aeabe8e160
Jaime Nazar Anchorena <jnazaranchorena@itba.edu.ar>
2026-03-11 20:43:10 -0300

    feat: Add base project

---
+++ webapp2026a/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc // Use all the defaults from webmvc
+@ComponentScan({ "ar.edu.itba.paw.webapp.controller" })
+@Configuration
+public class WebConfig {
+
+
+}
