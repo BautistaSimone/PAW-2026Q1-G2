e09399d686e8ef90cf6d79f5f1377ac348823fe9
Jaime Nazar Anchorena <jnazaranchorena@itba.edu.ar>
2026-03-30 12:27:33 -0300

    feat: Add unit tests

--- persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfiguration.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfiguration.java
@@ -1,17 +1,28 @@
 package ar.edu.itba.paw.persistence;
 
-import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
 
+import org.hsqldb.jdbc.JDBCDriver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+@ComponentScan({ "ar.edu.itba.paw.persistence" })
 @Configuration
 public class TestConfiguration {
-
 
     @Bean
     public DataSource dataSource() {
         final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(JDBCDriver.class);
-        dataSource.setUrl("jdbc:hsqldb:mem:paw");
-        dataSource.setUsername("sa");
+        dataSource.setUrl("jdbc:hsqldb:mem:paw;sql.syntax_pgs=true"); // Specify postgresql syntax
+        dataSource.setUsername("ha");
         dataSource.setPassword("");
 
         return dataSource;
--- persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
@@ -1,11 +1,32 @@
 package ar.edu.itba.paw.persistence;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.itba.paw.models.User;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfiguration.class)
 public class UserJdbcDaoTest {
     private UserJdbcDao userDao;
 
+    @Test
+    public void testCreateUserWhenUserDoesNotExist() {
+        // 1. Arrange
+        final String email = "[EMAIL_ADDRESS]";
+        final String password = "[PASSWORD]";
+        final String username = "[USERNAME]";
+
+        // 2. Exercise
+        final User user = userDao.createUser(email, password, username);
+
+        // 3. Assert
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(username, user.getUsername());
+        Assertions.assertEquals(password, user.getPassword());
+    }
 }
