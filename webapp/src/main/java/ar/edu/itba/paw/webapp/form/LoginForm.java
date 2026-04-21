ad46b9996bd7ef70f34d1161a108199d58d64df1
Jaime Nazar Anchorena <nam.jaime@gmail.com>
2026-04-21 13:32:45 -0300

    chore: Edit login form

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/LoginForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/LoginForm.java
@@ -14,8 +14,9 @@ public class LoginForm {
     @Size(max = 254)
     private String email;
 
-    @NotBlank(message = "{NotBlank.authForm.password}")
-    @Size(min = 8, max = 100, message = "{Size.authForm.password}")
+    // TODO: Volver a agregar una vez todos los usuarios hayan cambiado la password
+    // @NotBlank(message = "{NotBlank.authForm.password}")
+    @Size(min = 0, max = 100, message = "{Size.authForm.password}")
     @Pattern(
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
         message = "{Pattern.authForm.password}"
